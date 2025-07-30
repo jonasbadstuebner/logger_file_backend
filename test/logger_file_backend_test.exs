c2ad811c685ceb1c287ba48da84097a03e34a3b1
Jonas Badstübner <jonas@jb.software>
2025-07-30 20:01:40 +0200

    test: fix flaky tests by using context for Logger setup

--- test/logger_file_backend_test.exs
+++ test/logger_file_backend_test.exs
@@ -2,51 +2,48 @@ defmodule LoggerFileBackendTest do
   use ExUnit.Case, async: false
   require Logger
 
-  @backend {LoggerFileBackend, :test}
-  @basedir "test/logs"
+  @moduletag :tmp_dir
+  @moduletag capture_log: true
 
   import LoggerFileBackend, only: [prune: 1, metadata_matches?: 2]
 
-  setup_all do
-    on_exit(fn ->
-      File.rm_rf!(@basedir)
-    end)
-  end
-
   setup context do
+    backend = {LoggerFileBackend, context.test}
     # We add and remove the backend here to avoid cross-test effects
-    Logger.add_backend(@backend, flush: true)
+    Logger.add_backend(backend, flush: true)
 
-    config(path: logfile(context, @basedir), level: :debug)
+    config(Map.put(context, :backend, backend), path: logfile(context), level: :debug)
 
     on_exit(fn ->
-      :ok = Logger.remove_backend(@backend)
+      :ok = Logger.remove_backend(backend)
     end)
+
+    {:ok, [backend: backend]}
   end
 
-  test "does not crash if path isn't set" do
-    config(path: nil)
+  test "does not crash if path isn't set", %{backend: backend} = context do
+    config(context, path: nil)
 
     Logger.debug("foo")
-    assert {:error, :already_present} = Logger.add_backend(@backend)
+    assert {:error, :already_present} = Logger.add_backend(backend)
   end
 
-  test "can configure metadata_filter" do
-    config(metadata_filter: [md_key: true])
+  test "can configure metadata_filter", context do
+    config(context, metadata_filter: [md_key: true])
     Logger.debug("shouldn't", md_key: false)
     Logger.debug("should", md_key: true)
-    refute log() =~ "shouldn't"
-    assert log() =~ "should"
-    config(metadata_filter: nil)
+    refute log(context) =~ "shouldn't"
+    assert log(context) =~ "should"
+    config(context, metadata_filter: nil)
   end
 
-  test "can configure metadata_reject" do
-    config(metadata_reject: [md_key: false])
+  test "can configure metadata_reject", context do
+    config(context, metadata_reject: [md_key: false])
     Logger.debug("shouldn't", md_key: false)
     Logger.debug("should", md_key: true)
-    refute log() =~ "shouldn't"
-    assert log() =~ "should"
-    config(metadata_reject: nil)
+    refute log(context) =~ "shouldn't"
+    assert log(context) =~ "should"
+    config(context, metadata_reject: nil)
   end
 
   test "metadata_matches?" do
@@ -66,16 +63,16 @@ defmodule LoggerFileBackendTest do
     assert metadata_matches?([a: 1], b: 1, a: 1) == false
   end
 
-  test "creates log file" do
-    refute File.exists?(path())
+  test "creates log file", context do
+    refute File.exists?(path(context))
     Logger.debug("this is a msg")
-    assert File.exists?(path())
-    assert log() =~ "this is a msg"
+    assert File.exists?(path(context))
+    assert log(context) =~ "this is a msg"
   end
 
-  test "can log utf8 chars" do
+  test "can log utf8 chars", context do
     Logger.debug("ß\uFFaa\u0222")
-    assert log() =~ "ßﾪȢ"
+    assert log(context) =~ "ßﾪȢ"
   end
 
   test "prune/1" do
@@ -86,89 +83,89 @@ defmodule LoggerFileBackendTest do
     assert prune([[] | []]) == [[]]
   end
 
-  test "prunes invalid utf-8 codepoints" do
+  test "prunes invalid utf-8 codepoints", context do
     Logger.debug(<<"hi", 233>>)
-    assert log() =~ "hi�"
+    assert log(context) =~ "hi�"
   end
 
-  test "can configure format" do
-    config(format: "$message [$level]\n")
+  test "can configure format", context do
+    config(context, format: "$message [$level]\n")
 
     Logger.debug("hello")
-    assert log() =~ "hello [debug]"
+    assert log(context) =~ "hello [debug]"
   end
 
-  test "can configure metadata" do
-    config(format: "$metadata$message\n", metadata: [:user_id, :auth])
+  test "can configure metadata", context do
+    config(context, format: "$metadata$message\n", metadata: [:user_id, :auth])
 
     Logger.debug("hello")
-    assert log() =~ "hello"
+    assert log(context) =~ "hello"
 
     Logger.metadata(auth: true)
     Logger.metadata(user_id: 11)
     Logger.metadata(user_id: 13)
 
     Logger.debug("hello")
-    assert log() =~ "user_id=13 auth=true hello"
+    assert log(context) =~ "user_id=13 auth=true hello"
   end
 
-  test "can configure level" do
-    config(level: :info)
+  test "can configure level", context do
+    config(context, level: :info)
 
     Logger.debug("hello")
-    refute File.exists?(path())
+    refute File.exists?(path(context))
   end
 
-  test "can configure path" do
+  test "can configure path", context do
     new_path = "test/logs/test.log.2"
-    config(path: new_path)
-    assert new_path == path()
+    config(context, path: new_path)
+    assert new_path == path(context)
   end
 
-  test "logs to new file after old file has been moved" do
-    config(format: "$message\n")
+  test "logs to new file after old file has been moved", context do
+    config(context, format: "$message\n")
 
     Logger.debug("foo")
     Logger.debug("bar")
-    assert log() == "foo\nbar\n"
+    assert log(context) == "foo\nbar\n"
 
-    {"", 0} = System.cmd("mv", [path(), path() <> ".1"])
+    {"", 0} = System.cmd("mv", [path(context), path(context) <> ".1"])
 
     Logger.debug("biz")
     Logger.debug("baz")
-    assert log() == "biz\nbaz\n"
+    assert log(context) == "biz\nbaz\n"
   end
 
-  test "closes old log file after log file has been moved" do
+  test "closes old log file after log file has been moved", context do
     Logger.debug("foo")
-    assert has_open(path())
+    assert has_open(path(context))
 
-    new_path = path() <> ".1"
-    {"", 0} = System.cmd("mv", [path(), new_path])
+    new_path = path(context) <> ".1"
+    {"", 0} = System.cmd("mv", [path(context), new_path])
 
     assert has_open(new_path)
 
     Logger.debug("bar")
 
-    assert has_open(path())
+    assert has_open(path(context))
     refute has_open(new_path)
   end
 
-  test "closes old log file after path has been changed" do
+  test "closes old log file after path has been changed", context do
     Logger.debug("foo")
-    assert has_open(path())
+    assert has_open(path(context))
 
-    org_path = path()
-    config(path: path() <> ".new")
+    org_path = path(context)
+    config(context, path: path(context) <> ".new")
 
     Logger.debug("bar")
-    assert has_open(path())
+    assert has_open(path(context))
     refute has_open(org_path)
   end
 
-  test "log file rotate" do
-    config(format: "$message\n")
-    config(rotate: %{max_bytes: 4, keep: 4})
+  test "log file rotate", context do
+    config(context, format: "$message\n")
+    config(context, rotate: %{max_bytes: 4, keep: 4})
 
     Logger.debug("rotate1")
     Logger.debug("rotate2")
@@ -177,7 +174,7 @@ defmodule LoggerFileBackendTest do
     Logger.debug("rotate5")
     Logger.debug("rotate6")
 
-    p = path()
+    p = path(context)
 
     assert File.read!("#{p}.4") == "rotate2\n"
     assert File.read!("#{p}.3") == "rotate3\n"
@@ -185,37 +182,37 @@ defmodule LoggerFileBackendTest do
     assert File.read!("#{p}.1") == "rotate5\n"
     assert File.read!(p) == "rotate6\n"
 
-    config(rotate: nil)
+    config(context, rotate: nil)
   end
 
-  test "log file not rotate" do
-    config(format: "$message\n")
-    config(rotate: %{max_bytes: 100, keep: 4})
+  test "log file not rotate", context do
+    config(context, format: "$message\n")
+    config(context, rotate: %{max_bytes: 100, keep: 4})
 
     words = ~w(rotate1 rotate2 rotate3 rotate4 rotate5 rotate6)
     words |> Enum.map(&Logger.debug(&1))
 
-    assert log() == Enum.join(words, "\n") <> "\n"
+    assert log(context) == Enum.join(words, "\n") <> "\n"
 
-    config(rotate: nil)
+    config(context, rotate: nil)
   end
 
-  test "Allow :all to metadata" do
-    config(format: "$metadata")
+  test "Allow :all to metadata", context do
+    config(context, format: "$metadata")
 
-    config(metadata: [])
+    config(context, metadata: [])
     Logger.debug("metadata", metadata1: "foo", metadata2: "bar")
-    assert log() == ""
+    assert log(context) == ""
 
-    config(metadata: [:metadata3])
+    config(context, metadata: [:metadata3])
     Logger.debug("metadata", metadata3: "foo", metadata4: "bar")
-    assert log() == "metadata3=foo "
+    assert log(context) == "metadata3=foo "
 
-    config(metadata: :all)
+    config(context, metadata: :all)
     Logger.debug("metadata", metadata5: "foo", metadata6: "bar")
 
     # Match separately for metadata5/metadata6 to avoid depending on order
-    contents = log()
+    contents = log(context)
     assert contents =~ "metadata5=foo"
     assert contents =~ "metadata6=bar"
   end
@@ -238,22 +235,22 @@ defmodule LoggerFileBackendTest do
     false
   end
 
-  defp path do
-    {:ok, path} = :gen_event.call(Logger, @backend, :path)
+  defp path(%{backend: backend}) do
+    {:ok, path} = :gen_event.call(Logger, backend, :path)
     path
   end
 
-  defp log do
-    File.read!(path())
+  defp log(context) do
+    File.read!(path(context))
   end
 
-  defp config(opts) do
-    :ok = Logger.configure_backend(@backend, opts)
+  defp config(%{backend: backend}, opts) do
+    :ok = Logger.configure_backend(backend, opts)
   end
 
-  defp logfile(context, basedir) do
+  defp logfile(%{tmp_dir: basedir, test: test_name}) do
     logfile =
-      context.test
+      test_name
       |> Atom.to_string()
       |> String.replace(" ", "_")
 
